f0860764bf6e9bd671711540c391b5551bd2bd1b
AlepsSO <148327442+AlepsSO@users.noreply.github.com>
2023-11-18 17:57:02 +0100

    Planteamiento ventana Gestión Playlists

--- AppMusicProject/src/umu/tds/vista/VentanaMain.java
+++ AppMusicProject/src/umu/tds/vista/VentanaMain.java
@@ -17,10 +17,12 @@ import java.awt.FlowLayout;
 import java.awt.CardLayout;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import javax.swing.JTextField;
 
 public class VentanaMain extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField textField;
 
 	/**
 	 * Launch the application.
@@ -113,6 +115,12 @@ public class VentanaMain extends JFrame {
 		panelBotonera.add(btnNewButton_2, gbc_btnNewButton_2);
 
 		JButton btnNewButton_3 = new JButton("Mis Playlists");
+		btnNewButton_3.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				CardLayout card = (CardLayout) panelCardLayout.getLayout();
+				card.show(panelCardLayout, "panelPlaylists");
+			}
+		});
 		btnNewButton_3.setHorizontalAlignment(SwingConstants.LEFT);
 		btnNewButton_3.setIcon(new ImageIcon(VentanaMain.class.getResource("/umu/tds/images/altavoz.png")));
 		btnNewButton_3.setFont(new Font("Tahoma", Font.BOLD, 14));
@@ -168,6 +176,45 @@ public class VentanaMain extends JFrame {
 
 		JPanel panelPlaylists = new JPanel();
 		panelCardLayout.add(panelPlaylists, "panelPlaylists");
+		GridBagLayout gbl_panelPlaylists = new GridBagLayout();
+		gbl_panelPlaylists.columnWidths = new int[]{10, 64, 60, 43, 10, 0};
+		gbl_panelPlaylists.rowHeights = new int[]{10, 24, 0, 0, 0};
+		gbl_panelPlaylists.columnWeights = new double[]{1.0, 0.0, 0.0, 0.0, 1.0, Double.MIN_VALUE};
+		gbl_panelPlaylists.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		panelPlaylists.setLayout(gbl_panelPlaylists);
+		
+		JLabel lblPanelPlaylists = new JLabel("Título: ");
+		GridBagConstraints gbc_lblPanelPlaylists = new GridBagConstraints();
+		gbc_lblPanelPlaylists.insets = new Insets(0, 0, 5, 5);
+		gbc_lblPanelPlaylists.anchor = GridBagConstraints.NORTH;
+		gbc_lblPanelPlaylists.gridx = 1;
+		gbc_lblPanelPlaylists.gridy = 1;
+		panelPlaylists.add(lblPanelPlaylists, gbc_lblPanelPlaylists);
+		
+		textField = new JTextField();
+		GridBagConstraints gbc_textField = new GridBagConstraints();
+		gbc_textField.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField.gridwidth = 2;
+		gbc_textField.insets = new Insets(0, 0, 5, 5);
+		gbc_textField.gridx = 2;
+		gbc_textField.gridy = 1;
+		panelPlaylists.add(textField, gbc_textField);
+		textField.setColumns(10);
+		
+		JButton btnNewButton_4 = new JButton("Crear");
+		GridBagConstraints gbc_btnNewButton_4 = new GridBagConstraints();
+		gbc_btnNewButton_4.insets = new Insets(0, 0, 5, 5);
+		gbc_btnNewButton_4.gridx = 2;
+		gbc_btnNewButton_4.gridy = 2;
+		panelPlaylists.add(btnNewButton_4, gbc_btnNewButton_4);
+		
+		JButton btnNewButton_5 = new JButton("Eliminar");
+		GridBagConstraints gbc_btnNewButton_5 = new GridBagConstraints();
+		gbc_btnNewButton_5.anchor = GridBagConstraints.WEST;
+		gbc_btnNewButton_5.insets = new Insets(0, 0, 5, 5);
+		gbc_btnNewButton_5.gridx = 3;
+		gbc_btnNewButton_5.gridy = 2;
+		panelPlaylists.add(btnNewButton_5, gbc_btnNewButton_5);
 	}
 
 }
