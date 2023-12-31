60fdefa18e76e5d98a1d3192301527ca1b02e028
Barlowe <67549375+Barlowe90@users.noreply.github.com>
2023-12-31 18:30:49 +0100

    Update AdaptadorCancionTDS.java

--- AppMusicProject/src/umu/tds/persistencia/AdaptadorCancionTDS.java
+++ AppMusicProject/src/umu/tds/persistencia/AdaptadorCancionTDS.java
@@ -9,13 +9,19 @@ import beans.Propiedad;
 import tds.driver.FactoriaServicioPersistencia;
 import tds.driver.ServicioPersistencia;
 import umu.tds.modelo.Cancion;
-import umu.tds.modelo.EstiloMusical;
-import umu.tds.modelo.Interprete;
 
 public class AdaptadorCancionTDS implements IAdaptadorCancionDAO {
 
 	private static ServicioPersistencia servPersistencia;
 	private static AdaptadorCancionTDS unicaInstancia = null;
+
+	private static final String CANCION = "cancion";
+	private static final String ID = "id";
+	private static final String TITULO = "titulo";
+	private static final String INTERPRETE = "interprete";
+	private static final String ESTILO_MUSICAL = "estiloMusical";
+	private static final String RUTA_CANCION = "rutaCancion";
+	private static final String NUM_REPRODUCCIONES = "numReproducciones";
 
 	public static AdaptadorCancionTDS getUnicaInstancia() {
 		if (unicaInstancia == null)
@@ -27,32 +33,34 @@ public class AdaptadorCancionTDS implements IAdaptadorCancionDAO {
 		servPersistencia = FactoriaServicioPersistencia.getInstance().getServicioPersistencia();
 	}
 
+	private Cancion entidadToCancion(Entidad eCancion) {
+		String titulo = servPersistencia.recuperarPropiedadEntidad(eCancion, TITULO);
+		String interprete = servPersistencia.recuperarPropiedadEntidad(eCancion, INTERPRETE);
+		String estiloMusical = servPersistencia.recuperarPropiedadEntidad(eCancion, ESTILO_MUSICAL);
+		String rutaCancion = servPersistencia.recuperarPropiedadEntidad(eCancion, RUTA_CANCION);
+
+		Cancion cancion = new Cancion(titulo, interprete, estiloMusical, rutaCancion);
+		cancion.setCodigo(eCancion.getId());
+
+		return cancion;
+	}
+
+	private Entidad cancionToEntidad(Cancion cancion) {
+		Entidad eCancion = new Entidad();
+		eCancion.setNombre(CANCION);
+
+		eCancion.setPropiedades(new ArrayList<Propiedad>(Arrays.asList(new Propiedad(TITULO, cancion.getTitulo()),
+				new Propiedad(INTERPRETE, cancion.getInterprete()), new Propiedad(ESTILO_MUSICAL, cancion.getEstilo()),
+				new Propiedad(RUTA_CANCION, cancion.getRutaCancion().toString()),
+				new Propiedad(NUM_REPRODUCCIONES, String.valueOf(cancion.getNumReproducciones())))));
+
+		return eCancion;
+	}
+
 	@Override
 	public void registrarCancion(Cancion cancion) {
-		Entidad eCancion = null;
-
-		try {
-			eCancion = servPersistencia.recuperarEntidad(cancion.getCodigo());
-		} catch (NullPointerException e) {
-		}
-
-		if (eCancion != null)
-			return;
-
-		// Crear entidad cancion
-		eCancion = new Entidad();
-		eCancion.setNombre("cancion");
-		eCancion.setPropiedades(new ArrayList<Propiedad>(Arrays.asList(new Propiedad("titulo", cancion.getTitulo()),
-				new Propiedad("interprete", String.valueOf(cancion.getInterprete())),
-//				new Propiedad("interprete", String.valueOf(cancion.getInterprete().getNombre())),
-//				new Propiedad("estiloMusical", String.valueOf(cancion.getEstilo().getNombreEstilo())),
-				new Propiedad("estiloMusical", String.valueOf(cancion.getEstilo())),
-				new Propiedad("rutaCancion", String.valueOf(cancion.getRutaCancion())),
-				new Propiedad("numReproducciones", String.valueOf(cancion.getNumReproducciones())))));
-
-		// Registrar entidad cancion
+		Entidad eCancion = this.cancionToEntidad(cancion);
 		eCancion = servPersistencia.registrarEntidad(eCancion);
-		// Asignar ID. Lo genera el servicio de persistencia
 		cancion.setCodigo(eCancion.getId());
 	}
 
@@ -67,17 +75,17 @@ public class AdaptadorCancionTDS implements IAdaptadorCancionDAO {
 		Entidad eCancion = servPersistencia.recuperarEntidad(cancion.getCodigo());
 
 		for (Propiedad prop : eCancion.getPropiedades()) {
-			if (prop.getNombre().equals("codigo")) {
+			if (prop.getNombre().equals(ID)) {
 				prop.setValor(String.valueOf(cancion.getCodigo()));
-			} else if (prop.getNombre().equals("titulo")) {
+			} else if (prop.getNombre().equals(TITULO)) {
 				prop.setValor(cancion.getTitulo());
-			} else if (prop.getNombre().equals("interprete")) {
+			} else if (prop.getNombre().equals(INTERPRETE)) {
 				prop.setValor(String.valueOf(cancion.getInterprete()));
-			} else if (prop.getNombre().equals("estiloMusical")) {
+			} else if (prop.getNombre().equals(ESTILO_MUSICAL)) {
 				prop.setValor(String.valueOf(cancion.getEstilo()));
-			} else if (prop.getNombre().equals("rutaCancion")) {
+			} else if (prop.getNombre().equals(RUTA_CANCION)) {
 				prop.setValor(String.valueOf(cancion.getRutaCancion()));
-			} else if (prop.getNombre().equals("numReproducciones")) {
+			} else if (prop.getNombre().equals(NUM_REPRODUCCIONES)) {
 				prop.setValor(String.valueOf(cancion.getNumReproducciones()));
 			}
 			servPersistencia.modificarPropiedad(prop);
@@ -86,41 +94,17 @@ public class AdaptadorCancionTDS implements IAdaptadorCancionDAO {
 
 	@Override
 	public Cancion getCancion(int key) {
-		Entidad eCancion;
-		String titulo;
-//		String interpreteS;
-		String interprete;
-		String estiloMusical;
-		String rutaCancion;
-		int numReproducciones;
-
-		eCancion = servPersistencia.recuperarEntidad(key);
-		titulo = servPersistencia.recuperarPropiedadEntidad(eCancion, "titulo");
-
-		// esta es una opción, pero se crearían más objetos de tipo Interprete
-//		interpreteS = servPersistencia.recuperarPropiedadEntidad(eCancion, "interprete");
-//		interprete = new Interprete(interpreteS);
-
-		// por el momento voy a cambiarlos a tipos String directamente hasta encontrar
-		// la manera de recuperar sus objetos sin necesidad de crear un adaptador
-		interprete = servPersistencia.recuperarPropiedadEntidad(eCancion, "interprete");
-		estiloMusical = servPersistencia.recuperarPropiedadEntidad(eCancion, "estiloMusical");
-
-		rutaCancion = servPersistencia.recuperarPropiedadEntidad(eCancion, "rutaCancion");
-		numReproducciones = Integer.parseInt(servPersistencia.recuperarPropiedadEntidad(eCancion, "numReproducciones"));
-
-		Cancion cancion = new Cancion(titulo, interprete, estiloMusical, rutaCancion);
-		cancion.setCodigo(key);
-		return cancion;
+		Entidad eCancion = servPersistencia.recuperarEntidad(key);
+		return entidadToCancion(eCancion);
 	}
 
 	@Override
 	public List<Cancion> getAllCanciones() {
+		List<Entidad> eCanciones = servPersistencia.recuperarEntidades(CANCION);
 		List<Cancion> canciones = new LinkedList<Cancion>();
-		List<Entidad> entidades = servPersistencia.recuperarEntidades("cancion");
 
-		for (Entidad eCancion : entidades) {
-			canciones.add(recuperaCancion(eCancion.getId()));
+		for (Entidad eCancion : eCanciones) {
+			canciones.add(getCancion(eCancion.getId()));
 
 		}
 		return canciones;
