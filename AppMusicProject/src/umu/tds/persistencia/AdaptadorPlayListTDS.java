3ecb067f8b1bb98d26a74cb69339bef2ac4ec681
Barlowe <67549375+Barlowe90@users.noreply.github.com>
2023-12-31 18:56:23 +0100

    Update AdaptadorPlayListTDS.java

--- AppMusicProject/src/umu/tds/persistencia/AdaptadorPlayListTDS.java
+++ AppMusicProject/src/umu/tds/persistencia/AdaptadorPlayListTDS.java
@@ -11,19 +11,19 @@ import umu.tds.modelo.Cancion;
 import umu.tds.modelo.PlayList;
 
 public class AdaptadorPlayListTDS implements IAdaptadorPlayListDAO {
-	
+
 	private static ServicioPersistencia servicioPersistencia;
 	private static AdaptadorPlayListTDS unicaInstancia = null;
-	
+
 	private static final String NOMBRE = "nombre";
-	
+
 	public static AdaptadorPlayListTDS getUnicaInstancia() {
-		if(unicaInstancia == null) {
+		if (unicaInstancia == null) {
 			unicaInstancia = new AdaptadorPlayListTDS();
 		}
 		return unicaInstancia;
 	}
-	
+
 	private AdaptadorPlayListTDS() {
 		servicioPersistencia = FactoriaServicioPersistencia.getInstance().getServicioPersistencia();
 	}
@@ -31,23 +31,23 @@ public class AdaptadorPlayListTDS implements IAdaptadorPlayListDAO {
 	private PlayList entidadToPlayList(Entidad ePlayList) {
 		String nombre = servicioPersistencia.recuperarPropiedadEntidad(ePlayList, NOMBRE);
 		List<Cancion> lista = new LinkedList<Cancion>();
-		
+
 		PlayList playlist = new PlayList(nombre, lista);
 		return playlist;
 	}
-	
+
 	private Entidad playlistToEntidad(PlayList playlist) {
 		Entidad ePlayList = new Entidad();
 		ePlayList.setNombre(NOMBRE);
-		//TODO como tratar la lista
-		
+		// TODO como tratar la lista
+
 		return ePlayList;
 	}
-	
+
 	@Override
 	public void registrarPlayList(PlayList playlist) {
 		Entidad ePlayList = this.playlistToEntidad(playlist);
-		ePlayList = servicioPersistencia.registrarEntidad(ePlayList);		
+		ePlayList = servicioPersistencia.registrarEntidad(ePlayList);
 	}
 
 	@Override
@@ -60,28 +60,26 @@ public class AdaptadorPlayListTDS implements IAdaptadorPlayListDAO {
 	public void updatePlayList(PlayList playlist) {
 		ArrayList<Entidad> ePlayList = servicioPersistencia.recuperarEntidades(playlist.getNombre());
 		Entidad entidad = ePlayList.get(0);
-		//TODO
+		// TODO
 	}
 
 	@Override
 	public PlayList getPlayList(int id) {
-		// TODO 
+		// TODO
 		return null;
 	}
 
 	@Override
 	public List<PlayList> getAllPlayLists() {
-		// TODO 
+		// TODO
 		return null;
 	}
-	
+
 	// Funcion auxiliar
-	
-	private List<Cancion> obtenerCancionesDesdeNombrePlayList(String nombre){
-		//TODO obtener la lista de canciones
-		
+
+	private List<Cancion> obtenerCancionesDesdeNombrePlayList(String nombre) {
+		// TODO obtener la lista de canciones
+		return null;
 	}
-	
-	
 
 }
