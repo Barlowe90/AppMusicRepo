d2aef0b32108f5bf275544bb64c135eaec22c42b
Barlowe <67549375+Barlowe90@users.noreply.github.com>
2024-01-03 00:03:06 +0100

    Update VentanaMain.java

--- AppMusicProject/src/umu/tds/vista/VentanaMain.java
+++ AppMusicProject/src/umu/tds/vista/VentanaMain.java
@@ -1,7 +1,7 @@
 package umu.tds.vista;
 
 import java.awt.BorderLayout;
-import java.awt.EventQueue;
+//import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
