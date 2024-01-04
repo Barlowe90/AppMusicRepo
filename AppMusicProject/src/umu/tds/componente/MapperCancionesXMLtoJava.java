c06e62d8b3dc4796461077aaba294e395b83ecea
Barlowe <67549375+Barlowe90@users.noreply.github.com>
2024-01-04 17:38:26 +0100

    Update MapperCancionesXMLtoJava.java
    Corrijo la clase

--- AppMusicProject/src/umu/tds/componente/MapperCancionesXMLtoJava.java
+++ AppMusicProject/src/umu/tds/componente/MapperCancionesXMLtoJava.java
@@ -1,24 +1,32 @@
 package umu.tds.componente;
 
 import java.io.File;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
-import org.eclipse.persistence.jaxb.JAXBContext;
-import org.eclipse.persistence.exceptions.JAXBException;
-import org.eclipse.persistence.internal.oxm.Unmarshaller;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
 
 public class MapperCancionesXMLtoJava {
-	public static Canciones cargarCanciones(String fichero) {
+
+	public static Canciones cargarCanciones(String fichero) throws URISyntaxException {
+
 		JAXBContext jc;
 		Canciones canciones = null;
+
 		try {
 			jc = JAXBContext.newInstance("umu.tds.componente");
 			Unmarshaller u = jc.createUnmarshaller();
-			File file = new File(fichero);
+			URL resourceUrl = MapperCancionesXMLtoJava.class.getClassLoader().getResource(fichero);
+			Path path = Paths.get(resourceUrl.toURI());
+			File file = path.toFile();
 			canciones = (Canciones) u.unmarshal(file);
 		} catch (JAXBException e) {
 			e.printStackTrace();
 		}
-		
 		return canciones;
 	}
 }
