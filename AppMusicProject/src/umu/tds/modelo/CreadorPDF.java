1e842dd66cd08c6bdd1db24efd5415fbe525c5a5
Barlowe <67549375+Barlowe90@users.noreply.github.com>
2024-01-12 22:27:59 +0100

    Update CreadorPDF.java
    Ahora saca un mejor formato para las playlist del usuario

--- AppMusicProject/src/umu/tds/modelo/CreadorPDF.java
+++ AppMusicProject/src/umu/tds/modelo/CreadorPDF.java
@@ -3,11 +3,13 @@ package umu.tds.modelo;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.List;
 
 import com.itextpdf.text.Document;
 import com.itextpdf.text.DocumentException;
 import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfPTable;
 import com.itextpdf.text.pdf.PdfWriter;
 
 public class CreadorPDF {
@@ -26,7 +28,7 @@ public class CreadorPDF {
 		return unicaInstancia;
 	}
 
-	public void crearPDF(Usuario usuario, List<PlayList> playlist) throws FileNotFoundException, DocumentException {
+	public void crearPDF(Usuario usuario, List<PlayList> playlists) throws FileNotFoundException, DocumentException {
 		String nick = usuario.getNick();
 		String rutaArchivo = NOMBRE_ARCHIVO + File.separator + nick + FORMATO_ARCHIVO;
 
@@ -36,16 +38,33 @@ public class CreadorPDF {
 			carpeta.mkdirs();
 		}
 
-		FileOutputStream archivo = new FileOutputStream(rutaArchivo);
-		Document documento = new Document();
-		PdfWriter.getInstance(documento, archivo);
-		documento.open();
-		documento.add(new Paragraph("Listado de las playlist del usuario " + nick));
-		documento.add(new Paragraph("Nombre de la playlist "));
-		documento.add(new Paragraph("Titulo"));
-		documento.add(new Paragraph("Intérprete"));
-		documento.add(new Paragraph("Estilo"));
-		documento.close();
+		try (FileOutputStream archivo = new FileOutputStream(rutaArchivo)) {
+			Document documento = new Document();
+			PdfWriter.getInstance(documento, archivo);
+			documento.open();
+			documento.add(new Paragraph("Listado de las playlists del usuario " + nick));
+
+			for (PlayList playlist : playlists) {
+				documento.add(new Paragraph("\nNombre de la playlist: " + playlist.getNombre()));
+
+				PdfPTable tabla = new PdfPTable(3);
+				tabla.addCell("Titulo");
+				tabla.addCell("Intérprete");
+				tabla.addCell("Estilo");
+
+				for (Cancion cancion : playlist.getCanciones()) {
+					tabla.addCell(cancion.getTitulo());
+					tabla.addCell(cancion.getInterprete());
+					tabla.addCell(cancion.getEstilo());
+				}
+
+				documento.add(tabla);
+			}
+
+			documento.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
